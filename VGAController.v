d1497ec597d81c884da05ca4d9f9db1d24a36e7b
Hunter Habersaat <hah50@duke.edu>
2024-12-03 16:35:19 -0500

    VGA Lab commit

---
+++ VGAController.v
@@ -0,0 +1,166 @@
+`timescale 1 ns/ 100 ps
+module VGAController(     
+	input clk, 			// 100 MHz System Clock
+	input reset, 		// Reset Signal
+	output hSync, 		// H Sync Signal
+	output vSync, 		// Veritcal Sync Signal
+	output [3:0] VGA_R,  // Red Signal Bits
+	output [3:0] VGA_G,  // Green Signal Bits
+	output [3:0] VGA_B,  // Blue Signal Bits
+	inout ps2_clk,
+	inout ps2_data,
+	input CPU_RESETN, BTNC, BTNU, BTNL, BTNR, BTND,
+	input [10:1] JD);
+	
+	// Lab Memory Files Location
+	localparam FILES_PATH = "C:/Users/hah50/Downloads/ece-350-tank-shooter/";
+	
+	reg [9:0] currX;
+	reg [10:0] currY;
+	
+	// Boolan for whether x,y in square:
+	wire isInSquare;
+	assign isInSquare = (x >= currX && x < currX + SPRITE_SIZE) && (y >= currY && y < currY + SPRITE_SIZE);
+	
+	// Always loop to update on active
+	always @ (posedge screenEnd) begin
+	   if (RIGHT || BTNR)
+	       if (currX + SPRITE_SIZE + 3 < VIDEO_WIDTH)
+	           currX <= currX + 3;
+	   if (LEFT || BTNL)
+	       if (currX-3 > 1)
+	           currX <= currX - 3;
+	end
+	always @ (posedge screenEnd) begin
+	   if (DOWN || BTND)
+	       if (currY + SPRITE_SIZE + 3 < VIDEO_HEIGHT)
+	           currY <= currY + 3;
+	   if (UP || BTNU)
+	       if (currY - 3 > 0)
+	           currY <= currY - 3;   
+	end
+	       
+	wire DOWN = JD[4];
+	wire RIGHT = JD[3];
+	wire LEFT = JD[2];
+	wire UP = JD[1];
+	
+	// Clock divider 100 MHz -> 25 MHz
+	wire clk25; // 25MHz clock
+
+	reg[1:0] pixCounter = 0;      // Pixel counter to divide the clock
+    assign clk25 = pixCounter[1]; // Set the clock high whenever the second bit (2) is high
+	always @(posedge clk) begin
+		pixCounter <= pixCounter + 1; // Since the reg is only 3 bits, it will reset every 8 cycles
+	end
+
+	// VGA Timing Generation for a Standard VGA Screen
+	localparam 
+		VIDEO_WIDTH = 640,  // Standard VGA Width
+		VIDEO_HEIGHT = 480, // Standard VGA Height
+		SPRITE_SIZE = 64;
+
+	wire active, screenEnd;
+	wire[9:0] x;
+	wire[10:0] y;
+	
+	VGATimingGenerator #(
+		.HEIGHT(VIDEO_HEIGHT), // Use the standard VGA Values
+		.WIDTH(VIDEO_WIDTH))
+	Display( 
+		.clk25(clk25),  	   // 25MHz Pixel Clock
+		.reset(reset),		   // Reset Signal
+		.screenEnd(screenEnd), // High for one cycle when between two frames
+		.active(active),	   // High when drawing pixels
+		.hSync(hSync),  	   // Set Generated H Signal
+		.vSync(vSync),		   // Set Generated V Signal
+		.x(x), 				   // X Coordinate (from left)
+		.y(y)); 			   // Y Coordinate (from top)	   
+
+	// Image Data to Map Pixel Location to Color Address
+	localparam 
+		PIXEL_COUNT = VIDEO_WIDTH*VIDEO_HEIGHT, 	             // Number of pixels on the screen
+		PIXEL_ADDRESS_WIDTH = $clog2(PIXEL_COUNT) + 1,           // Use built in log2 command
+		BITS_PER_COLOR = 12, 	  								 // Nexys A7 uses 12 bits/color
+		PALETTE_COLOR_COUNT = 256, 								 // Number of Colors available
+		PALETTE_ADDRESS_WIDTH = $clog2(PALETTE_COLOR_COUNT) + 1; // Use built in log2 Command
+
+	wire[PIXEL_ADDRESS_WIDTH-1:0] imgAddress;  	 // Image address for the image data
+	wire[PALETTE_ADDRESS_WIDTH-1:0] colorAddr; 	 // Color address for the color palette
+	assign imgAddress = x + 640*y;				 // Address calculated coordinate
+
+	
+	
+	// Image Data to Map Pixel Location to Color Address
+	localparam 
+		SPRITE_PIXEL_COUNT =  SPRITE_SIZE * SPRITE_SIZE, 	             // Number of pixels on the screen
+		SPRITE_PIXEL_ADDRESS_WIDTH = $clog2(SPRITE_PIXEL_COUNT) + 1;     // Use built in log2 command
+	
+    wire [9:0] sprite_x = x - currX;
+    wire [10:0] sprite_y = y - currY;
+    
+    wire [SPRITE_PIXEL_ADDRESS_WIDTH-1:0] spriteAddress = sprite_x + (SPRITE_SIZE * sprite_y);				 // Address calculated active
+    wire [PALETTE_ADDRESS_WIDTH-1:0] spriteColorAddr;
+	
+	RAM #(		
+		.DEPTH(SPRITE_PIXEL_COUNT), 		            // Set RAM depth to contain every pixel
+		.DATA_WIDTH(PALETTE_ADDRESS_WIDTH),             // Set data width according to the color palette
+		.ADDRESS_WIDTH(SPRITE_PIXEL_ADDRESS_WIDTH),     // Set address with according to the pixel count
+		.MEMFILE({FILES_PATH, "p1Tankimage.mem"}))            // Memory initialization
+	ImageData2(
+		.clk(clk), 						         // Falling edge of the 100 MHz clk
+		.addr(spriteAddress),					 // Image data address
+		.dataOut(spriteColorAddr),				 // Color palette address
+		.wEn(1'b0)); 						     // We're always reading
+	
+	// Color Palette to Map Color Address to 12-Bit Color
+	wire[BITS_PER_COLOR-1:0] spriteColorData; // 12-bit color data at current pixel
+	
+	RAM #(
+		.DEPTH(PALETTE_COLOR_COUNT), 		       // Set depth to contain every color		
+		.DATA_WIDTH(BITS_PER_COLOR), 		       // Set data width according to the bits per color
+		.ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH),     // Set address width according to the color count
+		.MEMFILE({FILES_PATH, "p1Tankcolors.mem"}))  // Memory initialization
+	ColorPalette2(
+		.clk(clk), 							   	   // Rising edge of the 100 MHz clk
+		.addr(spriteColorAddr),					   // Address from the ImageData RAM
+		.dataOut(spriteColorData),				   // Color at current pixel
+		.wEn(1'b0)); 						       // We're always reading
+	
+	
+	
+	
+
+	RAM #(		
+		.DEPTH(PIXEL_COUNT), 				     // Set RAM depth to contain every pixel
+		.DATA_WIDTH(PALETTE_ADDRESS_WIDTH),      // Set data width according to the color palette
+		.ADDRESS_WIDTH(PIXEL_ADDRESS_WIDTH),     // Set address with according to the pixel count
+		.MEMFILE({FILES_PATH, "image.mem"})) // Memory initialization
+	ImageData (
+		.clk(clk), 						 // Falling edge of the 100 MHz clk
+		.addr(imgAddress),					 // Image data address
+		.dataOut(colorAddr),				 // Color palette address
+		.wEn(1'b0)); 						 // We're always reading
+
+	// Color Palette to Map Color Address to 12-Bit Color
+	wire[BITS_PER_COLOR-1:0] colorData; // 12-bit color data at current pixel
+
+	RAM #(
+		.DEPTH(PALETTE_COLOR_COUNT), 		       // Set depth to contain every color		
+		.DATA_WIDTH(BITS_PER_COLOR), 		       // Set data width according to the bits per color
+		.ADDRESS_WIDTH(PALETTE_ADDRESS_WIDTH),     // Set address width according to the color count
+		.MEMFILE({FILES_PATH, "colors.mem"}))  // Memory initialization
+	ColorPalette (
+		.clk(clk), 							   	   // Rising edge of the 100 MHz clk
+		.addr(colorAddr),					       // Address from the ImageData RAM
+		.dataOut(colorData),				       // Color at current pixel
+		.wEn(1'b0)); 						       // We're always reading
+	
+
+	// Assign to output color from register if active
+	wire[BITS_PER_COLOR-1:0] colorOut; 			  // Output color 
+	assign colorOut = active ? (isInSquare ? spriteColorData : colorData) : 12'd0; // When not active, output black
+
+	// Quickly assign the output colors to their channels using concatenation
+	assign {VGA_R, VGA_G, VGA_B} = colorOut;
+endmodule
---
+++ constraints.xdc
@@ -0,0 +1,80 @@
+// Clock on E3
+set_property PACKAGE_PIN E3 [get_ports clk]
+set_property IOSTANDARD LVCMOS33 [get_ports clk]
+
+// Rest Signal
+set_property PACKAGE_PIN N17 [get_ports reset]
+set_property IOSTANDARD LVCMOS33 [get_ports reset]
+
+// VGA Port
+set_property PACKAGE_PIN D8 [get_ports {VGA_B[3]}]
+set_property PACKAGE_PIN D7 [get_ports {VGA_B[2]}]
+set_property PACKAGE_PIN C7 [get_ports {VGA_B[1]}]
+set_property PACKAGE_PIN B7 [get_ports {VGA_B[0]}]
+set_property PACKAGE_PIN A6 [get_ports {VGA_G[3]}]
+set_property PACKAGE_PIN B6 [get_ports {VGA_G[2]}]
+set_property PACKAGE_PIN A5 [get_ports {VGA_G[1]}]
+set_property PACKAGE_PIN C6 [get_ports {VGA_G[0]}]
+set_property PACKAGE_PIN A4 [get_ports {VGA_R[3]}]
+set_property PACKAGE_PIN C5 [get_ports {VGA_R[2]}]
+set_property PACKAGE_PIN B4 [get_ports {VGA_R[1]}]
+set_property PACKAGE_PIN A3 [get_ports {VGA_R[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_B[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_B[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_B[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_B[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_G[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_G[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_G[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_G[0]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_R[3]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_R[2]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_R[1]}]
+set_property IOSTANDARD LVCMOS33 [get_ports {VGA_R[0]}]
+
+// Sync Ports
+set_property PACKAGE_PIN B11 [get_ports hSync]
+set_property PACKAGE_PIN B12 [get_ports vSync]
+set_property IOSTANDARD LVCMOS33 [get_ports hSync]
+set_property IOSTANDARD LVCMOS33 [get_ports vSync]
+
+// PS2 Stuff
+set_property PACKAGE_PIN F4 [get_ports ps2_clk]
+set_property PACKAGE_PIN B2 [get_ports ps2_data]
+set_property IOSTANDARD LVCMOS33 [get_ports ps2_clk]
+set_property IOSTANDARD LVCMOS33 [get_ports ps2_data]
+
+#Buttons
+set_property -dict { PACKAGE_PIN C12   IOSTANDARD LVCMOS33 } [get_ports { CPU_RESETN }]; #IO_L3P_T0_DQS_AD1P_15 Sch=cpu_resetn
+set_property -dict { PACKAGE_PIN N17   IOSTANDARD LVCMOS33 } [get_ports { BTNC }]; #IO_L9P_T1_DQS_14 Sch=btnc
+set_property -dict { PACKAGE_PIN M18   IOSTANDARD LVCMOS33 } [get_ports { BTNU }]; #IO_L4N_T0_D05_14 Sch=btnu
+set_property -dict { PACKAGE_PIN P17   IOSTANDARD LVCMOS33 } [get_ports { BTNL }]; #IO_L12P_T1_MRCC_14 Sch=btnl
+set_property -dict { PACKAGE_PIN M17   IOSTANDARD LVCMOS33 } [get_ports { BTNR }]; #IO_L10N_T1_D15_14 Sch=btnr
+set_property -dict { PACKAGE_PIN P18   IOSTANDARD LVCMOS33 } [get_ports { BTND }]; #IO_L9N_T1_DQS_D13_14 Sch=btnd
+
+##Pmod Headers
+##Pmod Header JC
+set_property -dict { PACKAGE_PIN K1    IOSTANDARD LVCMOS33 } [get_ports { JC[1] }]; #IO_L23N_T3_35 Sch=jc[1]
+set_property -dict { PACKAGE_PIN F6    IOSTANDARD LVCMOS33 } [get_ports { JC[2] }]; #IO_L19N_T3_VREF_35 Sch=jc[2]
+set_property -dict { PACKAGE_PIN J2    IOSTANDARD LVCMOS33 } [get_ports { JC[3] }]; #IO_L22N_T3_35 Sch=jc[3]
+set_property -dict { PACKAGE_PIN G6    IOSTANDARD LVCMOS33 } [get_ports { JC[4] }]; #IO_L19P_T3_35 Sch=jc[4]
+set_property -dict { PACKAGE_PIN E7    IOSTANDARD LVCMOS33 } [get_ports { JC[7] }]; #IO_L6P_T0_35 Sch=jc[7]
+set_property -dict { PACKAGE_PIN J3    IOSTANDARD LVCMOS33 } [get_ports { JC[8] }]; #IO_L22P_T3_35 Sch=jc[8]
+set_property -dict { PACKAGE_PIN J4    IOSTANDARD LVCMOS33 } [get_ports { JC[9] }]; #IO_L21P_T3_DQS_35 Sch=jc[9]
+set_property -dict { PACKAGE_PIN E6    IOSTANDARD LVCMOS33 } [get_ports { JC[10] }]; #IO_L5P_T0_AD13P_35 Sch=jc[10]
+
+##Pmod Header JD
+set_property -dict { PACKAGE_PIN H4    IOSTANDARD LVCMOS33 } [get_ports { JD[1] }]; #IO_L21N_T3_DQS_35 Sch=jd[1]
+set_property -dict { PACKAGE_PIN H1    IOSTANDARD LVCMOS33 } [get_ports { JD[2] }]; #IO_L17P_T2_35 Sch=jd[2]
+set_property -dict { PACKAGE_PIN G1    IOSTANDARD LVCMOS33 } [get_ports { JD[3] }]; #IO_L17N_T2_35 Sch=jd[3]
+set_property -dict { PACKAGE_PIN G3    IOSTANDARD LVCMOS33 } [get_ports { JD[4] }]; #IO_L20N_T3_35 Sch=jd[4]
+set_property -dict { PACKAGE_PIN H2    IOSTANDARD LVCMOS33 } [get_ports { JD[7] }]; #IO_L15P_T2_DQS_35 Sch=jd[7]
+set_property -dict { PACKAGE_PIN G4    IOSTANDARD LVCMOS33 } [get_ports { JD[8] }]; #IO_L20P_T3_35 Sch=jd[8]
+set_property -dict { PACKAGE_PIN G2    IOSTANDARD LVCMOS33 } [get_ports { JD[9] }]; #IO_L15N_T2_DQS_35 Sch=jd[9]
+set_property -dict { PACKAGE_PIN F3    IOSTANDARD LVCMOS33 } [get_ports { JD[10] }]; #IO_L13N_T2_MRCC_35 Sch=jd[10]
+
+
+
+
+
+
