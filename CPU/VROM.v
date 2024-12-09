be2bc60d0a2fb38b785944dd78ad67d310ee3b45
Hunter Habersaat <hunter.habersaat@duke.edu>
2024-12-09 05:49:47 -0500

    VROM added

---
+++ CPU/VROM.v
@@ -0,0 +1,18 @@
+`timescale 1ns / 1ps
+module VROM #( parameter DATA_WIDTH = 8, ADDRESS_WIDTH = 8, DEPTH = 256, MEMFILE = "") (
+    input wire                     clk,
+    input wire [ADDRESS_WIDTH-1:0] addr,
+    output reg [DATA_WIDTH-1:0]    dataOut);
+    
+    reg[DATA_WIDTH-1:0] MemoryArray[0:DEPTH-1];
+    
+    initial begin
+        if(MEMFILE > 0) begin
+            $readmemh(MEMFILE, MemoryArray);
+        end
+    end
+    
+    always @(posedge clk) begin
+        dataOut <= MemoryArray[addr];
+    end
+endmodule
